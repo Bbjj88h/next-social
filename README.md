5ce69e9ceeae1461ab8a9c5b68823c16582cecc9
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-06-20 02:07:11 +1200

    Initial commit - setup docker compose and NextJS and Express apps

---
+++ README.md
@@ -0,0 +1,2 @@
+Next Social
+A very basic social media application with NextJS and MySQL.
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next Social",
+  description: "A basic social media website made with Next.js and MySQL",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
